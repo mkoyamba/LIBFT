7874a73614c08c6ac3f105eee3343273a8119937
Maxime Koyamba <mkoyamba@fu-r7-p3.s19.be>
2021-10-04 10:01:27 +0200

    strlcpypatch

--- srcs/string/ft_strlcpy.c
+++ srcs/string/ft_strlcpy.c
@@ -6,33 +6,23 @@
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:33:31 by mkoyamba          #+#    #+#             */
-/*   Updated: 2021/10/04 09:41:53 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2021/10/04 10:00:59 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-#include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	unsigned int	n;
 
 	n = 0;
-	if (dstsize == 0)
-		return (NULL);
-	else
+	while (src[n] != '\0' && n < dstsize - 1 && dstsize != 0)
 	{
-		while (src[n] != '\0' && n < dstsize)
-		{
-			dst[n] = src[n];
-			n++;
-		}
-		dst[n] = '\0';
+		dst[n] = src[n];
+		n++;
 	}
+	if (dstsize != 0)
+		dst[n] = '\0';
 	return ((size_t)ft_strlen(src));
-}
-
-int	main(void)
-{
-	
 }

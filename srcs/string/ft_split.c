89df6de2f2084a76a31a96bb439085c004e2b22e
Maxime Koyamba <mkoyamba@fu-r8-p3.s19.be>
2021-10-05 08:45:18 +0200

    SPLITDONE

--- srcs/string/ft_split.c
+++ srcs/string/ft_split.c
@@ -6,12 +6,11 @@
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 10:30:32 by mkoyamba          #+#    #+#             */
-/*   Updated: 2021/10/04 14:39:49 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2021/10/05 08:43:35 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include "wraloc.h"
 
 void	mk_free(char **result, int a)
 {
@@ -32,25 +31,21 @@ static int	mk_word(const char *s, char c)
 	int	n;
 
 	i = 0;
-	n = 1;
+	n = 0;
 	if (!s || s[0] == '\0')
 		return (0);
 	while (s[i] != '\0')
 	{
+		if (c == '\0' || s[i] == '\0')
+			return (1);
+		if (s[i + 1] == '\0')
+			break ;
+		if (s[i] != '\0' && s[i] != c)
+			n++;
 		while (s[i] != '\0' && s[i] != c)
 			i++;
-		if (i == 0)
+		while (s[i] == c)
 			i++;
-		if (s[i] == c)
-		{
-			if (c == '\0' || s[i + 1] == '\0')
-				break ;
-			else
-			{
-				i++;
-				n++;
-			}
-		}
 	}
 	return (n);
 }
@@ -62,8 +57,10 @@ char	*mk_strpush(const char *s, char c, int *n)
 	char	*tab;
 	int		len;
 
-	i = *n;
 	g = 0;
+	while (s[*n] != '\0' && s[*n] == c)
+		*n = *n + 1;
+	i = *n;
 	while (s[i] != '\0' && s[i] != c)
 		i++;
 	len = i - *n;
@@ -76,8 +73,7 @@ char	*mk_strpush(const char *s, char c, int *n)
 		g++;
 		*n = *n + 1;
 	}
-	*n = *n + 1;
-	tab[i] = '\0';
+	tab[g] = '\0';
 	return (tab);
 }
 
@@ -104,24 +100,4 @@ char	**ft_split(char const *s, char c)
 	}
 	result[a] = NULL;
 	return (result);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	int		a;
-	char	**result;
-
-	a = 0;
-	result = ft_split("cabcdefcghcij", 'c');
-	while (result[a] != NULL)
-	{
-		printf("%s\n", result[a]);
-		free(result[a]);
-		a++;
-	}
-	printf("%s\n", result[a]);
-	free(result);
-	return (0);
 }

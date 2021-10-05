bc729196585f52e65e33baeb903d6b0e80ac4cb1
Maxime Koyamba <mkoyamba@fu-r8-p3.s19.be>
2021-10-05 12:17:41 +0200

    updateLIBFT

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 20:34:28 by mkoyamba          #+#    #+#             */
-/*   Updated: 2021/10/05 11:45:55 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2021/10/05 12:15:00 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,14 @@ int		ft_toupper(int c);
 
 /*  	STRING	  */
 
-char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *restrict dst,
 			const char *restrict src, size_t dstsize);
 int		ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 /*  	STDLIB	  */
 
@@ -53,9 +49,14 @@ int		ft_atoi(const char *str);
 
 /*  	ABS		  */
 
+char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
-char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif

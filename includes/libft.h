d29d9bd620f3c1afe25685b65d6ed5aaff045c3c
Maxime Koyamba <mkoyamba@fu-r8-p3.s19.be>
2021-10-05 10:25:31 +0200

    updateLIBFT

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 20:34:28 by mkoyamba          #+#    #+#             */
-/*   Updated: 2021/10/04 09:34:06 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2021/10/05 10:24:57 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,17 @@ int		ft_toupper(int c);
 
 /*  	STRING	  */
 
+char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *restrict dst,
 			const char *restrict src, size_t dstsize);
 int		ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
 
 /*  	STDLIB	  */
 

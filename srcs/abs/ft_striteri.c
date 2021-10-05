94176fc0091aa3fd73485702e835ba15480df92a
Maxime Koyamba <mkoyamba@fu-r8-p3.s19.be>
2021-10-05 13:46:27 +0200

    striteri

--- srcs/abs/ft_striteri.c
+++ srcs/abs/ft_striteri.c
@@ -1,37 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/05 13:17:26 by mkoyamba          #+#    #+#             */
-/*   Updated: 2021/10/05 13:42:08 by mkoyamba         ###   ########.fr       */
+/*   Created: 2021/10/05 13:33:39 by mkoyamba          #+#    #+#             */
+/*   Updated: 2021/10/05 13:46:13 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	n;
-	char			*result;
 
 	n = 0;
-	if (!s || s[0] == '\0')
-		return (NULL);
 	if (!f)
-	{
-		result = ft_strdup(s);
-		return (result);
-	}
-	result = malloc((ft_strlen(s) + 1) * sizeof(char));
-	if (!result)
-		return (NULL);
+		return ;
+	if (!s || s[0] == '\0')
+		return ;
 	while (s[n])
 	{
-		result[n] = f(n, s[n]);
+		f(n, &s[n]);
 		n++;
 	}
-	return (result);
 }

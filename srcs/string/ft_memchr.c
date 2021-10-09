d5243626a149eb4c660609635c979a51fc5d776a
Maxime Koyamba <mkoyamba@fu-r2-p3.s19.be>
2021-10-09 09:53:37 +0200

    memchr

---
+++ srcs/string/ft_memchr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 09:44:52 by mkoyamba          #+#    #+#             */
+/*   Updated: 2021/10/09 09:53:28 by mkoyamba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char			*str;
+	unsigned char	search;
+	size_t			i;
+
+	str = (char *)s;
+	search = (unsigned char)c;
+	i = 0;
+	if (n == 0 || !s || !c)
+		return (NULL);
+	while (i < n)
+	{
+		if (str[i] == search)
+			return ((void *)s + i);
+		else
+			i++;
+	}
+	return (NULL);
+}
